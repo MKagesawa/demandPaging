7079f089bee178fab5ffad225aa6d29e7dd72972
Masaki <mk5986@nyu.edu>
2019-04-21 17:01:11 -0400

    Process class

--- demandPaging.py
+++ demandPaging.py
@@ -16,4 +16,57 @@ print("The number of references per process is " + numRef + ".")
 print("The replacement algorithm is " + replacementAlgo + ".")
 print("The level of debugging output is " + debugLevel + ".")
 
+randomNumbers = None
+with open("random-numbers.txt") as f:
+    randomNumbers = f.readlines()
+
+counter = 0
+def randomOS(counter):
+    num = int(randomNumbers[int(counter)])
+    if debugLevel == "11":
+        print(int(randomNumbers[int(counter)]))
+    return num
+
+
+class Process:
+    def __init__(self, processSize, processNum):
+        self.processSize = processSize
+        self.numFault = 0
+        self.numEvict = 0
+        self.resTime = 0
+        self.nextRef = (111 * processNum) % processSize
+
+    def nextRef(self, A, B, C):
+        global counter
+        randomNum = randomOS(counter)
+        counter += 1
+        ratio = randomNum / 2147483648
+        if ratio < A:
+            self.nextRef = (self.nextRef + 1) % processSize
+        elif ratio < A + B:
+            self.nextRef = (self.nextRef -5 + processSize) % processSize
+        elif ratio < A + B + C:
+            self.nextRef = (self.nextRef + 4) % processSize
+        else:
+            self.nextRef = randomNum % processSize
+            counter += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
